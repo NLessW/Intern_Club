3a055ef8686d41b0fd4af77e8085a9367284a6f1
Less <63160418+NLessW@users.noreply.github.com>
2024-04-04 02:54:16 +0900

    Update Pronunciation.cpp

--- 2024_04/0403/beginner/Pronunciation.cpp
+++ 2024_04/0403/beginner/Pronunciation.cpp
@@ -5,14 +5,13 @@ using namespace std;
 
 int solution(int angle) {
     int answer = 0;
-       if(angle>0 && angle<90){
+    if(angle>0 && angle<90){
         return 1;
-}else if(angle==90){
+    }else if(angle==90){
         return 2;
     }else if(angle>90 && angle<180){
         return 3;
-    }
-    else if(angle==180){
+    }else if(angle==180){
         return 4;
     }
     return answer;
