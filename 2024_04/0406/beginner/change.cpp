42f4cb770cc9dbedcd21b6c5267bba3aa08a3070
Less <63160418+NLessW@users.noreply.github.com>
2024-04-07 14:06:40 +0900

    Update change.cpp

--- 2024_04/0406/beginner/change.cpp
+++ 2024_04/0406/beginner/change.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -6,17 +7,14 @@ int main() {
     int defaultMoney = 1000;
     int changesMoney = 0;
     int count = 0;
-    int i = 0;
-    int coin[6] = {500, 100, 50, 10, 5, 1};
+    vector<int> coin = {500, 100, 50, 10, 5, 1};
     cin >> changesMoney;
     defaultMoney -= changesMoney;
 
-    while (defaultMoney > 0) {
-        if (defaultMoney >= coin[i]) {
+    for (int i = 0; defaultMoney > 0 && i < coin.size(); ++i) {
+        while (defaultMoney >= coin[i]) {
             defaultMoney -= coin[i];
             count++;
-        } else {
-            i++;
         }
     }
 
