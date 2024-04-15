d14f875acfbdbe026d1a9013d92247accf0ca32e
Less <63160418+NLessW@users.noreply.github.com>
2024-04-15 22:35:56 +0900

    Create indexChange.java

---
+++ 2024_04/0415/beginner/indexChange.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        String answer = "";
+        char[] charArray = my_string.toCharArray();
+        
+        char temp = charArray[num1];
+        charArray[num1] = charArray[num2];
+        charArray[num2] = temp;
+        
+        answer = new String(charArray);
+        
+        return answer;
+    }
+}
